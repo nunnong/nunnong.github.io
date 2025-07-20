950331db73598ff12c2fbcd645c66523e4ee953d
nunnong <nunnong@users.noreply.github.com>
2025-07-20 05:31:28 +0000

    deploy: 6abaa7c772253970c3c12a73ebf5dfe4be2133f5

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752989022',resources: [
+    cacheName: 'chirpy-1752989487',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
